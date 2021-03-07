a03341fa19178e6694eb259b0a140d96c561364c
rob <dev@mexion.com>
2021-03-07 10:42:26 +0100

    translations

--- lang/nl/_index.js
+++ lang/nl/_index.js
@@ -2,10 +2,14 @@ import email from './email.json';
 import action from './action.json';
 import mazui from './mazui.json';
 import validations from './validations.json';
+import misc from './misc.json';
+import dashboard from './dashboard.json';
 
 export default {
   email,
   action,
   validations,
-  mazui
+  mazui,
+  misc,
+  dashboard
 };
